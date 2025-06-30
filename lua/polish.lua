f61f4cdab761d5b3f795968cbc5bddcf1ee764ce
rainewallis <raine@rainew.dev>
2025-06-30 23:13:40 +0100

    Add editorconfig file

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,9 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Set filetype for .editorconfig files to ini to enable
+-- TreeSitter syntax highlighting
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = ".editorconfig", command = "setfiletype ini" })
